aae3a3ec36fc73ac3342fc6c4f7b634da6806ff5
Himselfem <141808913+ATO99Y@users.noreply.github.com>
2024-07-24 17:26:56 +0800

    Create Shellversion.sh
    Please don't view this This is only Write only Termux and Linux

---
+++ Shellversion.sh
@@ -0,0 +1,50 @@
+
+#Why are you viewing this#Why are you viewing this#Why are you viewing this#Why are you viewing this#Why are you viewing this#Why are you viewing this
+#Why are you viewing this
+#Why are you viewing this
+#Why are you viewing this
+#Why are you viewing this
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#!/bin/bash
+
+echo -e "\033[31m=================================================="
+echo -e "\033[31mCode expired: An unexpected error has occurred."
+echo -e "\033[31mPlease contact the system administrator for assistance."
+echo -e "\033[31m=================================================="
+echo ""
+echo "Error details:"
+echo ""
+echo -e "\033[31m  Error code: 0xDEADBEEF"
+echo -e "\033[31m  Error message: Unknown error"
+echo "  File: technical_error.sh"
+echo "  Line: 13"
+echo "  Function: show_Login_screen"
+echo ""
+echo "Technical support:"
+echo "  Phone: 555-555-1212"
+echo "  Email: And&andDev@gmail.com"
+echo "  Website: Http://and&and.com"
+echo ""
+echo -e "\033[37mCopyright (c) 2023 Onix Crew. All rights reserved."
+echo ""
